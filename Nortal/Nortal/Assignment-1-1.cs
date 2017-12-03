13c2e7bc137d67e678f9b2b2afdecdbdb5c2b362
Zylooh <Hoolyz@gmail.com>
2017-12-03 15:55:20 +0200

    Nortal visual studio assignment

---
+++ Nortal/Nortal/Assignment-1-1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nortal
+{
+    class Assignment1
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(palauta());
+        //    Console.ReadLine();
+        //}
+       
+        static List<int> palauta()
+        {
+
+            List<int> testi1 = new List<int>();
+
+            for(int i = 0;i <= 100; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0)
+                {
+                
+                }
+                else if(i % 5 == 0)
+                {
+                    testi1.Add(i);
+                }
+                else if (i % 3 == 0)
+                {
+                    testi1.Add(i);
+                }
+            }
+
+            return testi1;
+        }
+
+    }
+}
---
+++ Nortal/Nortal/Assignment-1-2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nortal
+{
+    class Assignment2
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Syötä ensimmäinen string");
+        //    string input1 = Console.ReadLine();
+        //    Console.WriteLine("Syöta toinen string");
+        //    string input2 = Console.ReadLine();
+
+           
+
+        //    if(AnagramChecker(input1, input2))
+        //    {
+        //        Console.WriteLine("Ovat anagrammeja");
+        //        Console.ReadLine();
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Eivät Ole");
+        //        Console.ReadLine();
+        //    }
+
+        //}
+
+
+        public static bool AnagramChecker(string yksi,string kaksi)
+        {
+            char[] arrayChar1 = yksi.ToUpper().ToCharArray();
+            char[] arrayChar2 = kaksi.ToUpper().ToCharArray();
+
+            Array.Sort(arrayChar1);
+            Array.Sort(arrayChar2);
+
+            string testi1 = new string(arrayChar1);
+            string testi2 = new string(arrayChar2);
+
+            if(testi1 == testi2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
